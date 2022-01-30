1f33e0ac812261326986ca2bd6cf4bc3351e9fa9
Kate Spitalnic <kespitalnic@gmail.com>
2022-01-30 00:08:34 -0500

    passing engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,11 +1,11 @@
 const Engineer = require('../lib/Engineer.js');
 
 it('should create an object for each engineer', () => {
-  const engineer = new Engineer('name', 401, 'email@gmail.com', 'gitHub');
+  const engineer = new Engineer('name', 401, 'email@gmail.com', 1, 'gitHub');
 
-  expect(engineer.name).toBe('myname');
-  expect(engineer.id).toEqual(0);
-  expect(engineer.email).toBe('myemail');
+  expect(engineer.name).toBe('name');
+  expect(engineer.id).toEqual(401);
+  expect(engineer.email).toBe('email@gmail.com');
   expect(engineer.phone).toBe(1);
-  expect(engineer.github).toBe('github')
+  expect(engineer.github).toBe('gitHub')
 });
