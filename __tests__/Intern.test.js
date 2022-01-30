56479f5f307cf02d554dbb506aca229259af1a08
Kate Spitalnic <kespitalnic@gmail.com>
2022-01-30 00:12:34 -0500

    all passing tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,11 +1,11 @@
 const Intern = require('../lib/Intern.js');
 
 it('should create an object for each intern', () => {
-  const intern = new Intern('name', 401, 'email@gmail.com', 'school');
+  const intern = new Intern('name', 401, 'email@gmail.com', 1, 'school');
 
-  expect(intern.name).toBe('myname');
-  expect(intern.id).toEqual(0);
-  expect(intern.email).toBe('myemail');
+  expect(intern.name).toBe('name');
+  expect(intern.id).toEqual(401);
+  expect(intern.email).toBe('email@gmail.com');
   expect(intern.phone).toBe(1);
   expect(intern.school).toBe('school')
 });
